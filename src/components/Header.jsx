d9696a1312079614a58f128e36d781c857b85dbd
PulakBala <pulakbala0524@gmail.com>
2025-01-08 22:56:15 +0600

    working header menu in mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setIsDropdownOpen(true);
@@ -10,6 +11,10 @@ function Header() {
 
   const handleMouseLeave = () => {
     setIsDropdownOpen(false);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
@@ -37,85 +42,110 @@ function Header() {
             <button
               className="navbar-toggler ms-auto"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
+              onClick={toggleSidebar}
               aria-controls="navbarNav"
-              aria-expanded="false"
+              aria-expanded={isSidebarOpen}
               aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto text-uppercase">
-                <li className="nav-item me-4">
-                  <Link to="/" className="nav-link" style={{ color: '#eb3d66' }}>
-                    About Us
-                  </Link>
-                </li>
-                {/* Dropdown for Products / Solutions */}
-                <li
-                  className="nav-item dropdown me-4"
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                  id="productSolutionsDropdown"
-                >
-                  <a
-                    href="#"
-                    className="nav-link"
-                    style={{ color: '#eb3d66' }}
+
+            {/* Mobile Sidebar */}
+            <div 
+              className={`offcanvas offcanvas-start ${isSidebarOpen ? 'show' : ''}`} 
+              tabIndex="-1" 
+              id="navbarNav"
+              style={{width: '250px'}}
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title">Menu</h5>
+                <button 
+                  type="button" 
+                  className="btn-close" 
+                  onClick={() => setIsSidebarOpen(false)}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav ms-auto text-uppercase">
+                  <li className="nav-item me-4">
+                    <Link to="/" className="nav-link" style={{ color: '#eb3d66' }}>
+                      About Us
+                    </Link>
+                  </li>
+                  {/* Dropdown for Products / Solutions */}
+                  <li
+                    className="nav-item dropdown me-4"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    id="productSolutionsDropdown"
                   >
-                    Products / Solutions
-                  </a>
-                  {isDropdownOpen && (
-                    <ul className="dropdown-menu show">
-                      <li>
-                        <Link to="/bio-degradable" className="dropdown-item">
-                          BIO DEGRADEABLE
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/offset-printing" className="dropdown-item">
-                          OFFSET PRINTING
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/advise" className="dropdown-item">
-                         AUTO LABEL (SELF ADHESIVE) PRINTING
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/corggoted-cartton" className="dropdown-item">
-                          CORROGATED CARTON
-                        </Link>
-                      </li>
-                    </ul>
-                  )}
-                </li>
-                <li className="nav-item me-4">
-                  <Link to="/sustainability" className="nav-link" style={{ color: '#eb3d66' }}>
-                    Sustainability
-                  </Link>
-                </li>
-                <li className="nav-item me-4">
-                  <Link to="/news-events" className="nav-link" style={{ color: '#eb3d66' }}>
-                    News & Events
-                  </Link>
-                </li>
-                <li className="nav-item me-4">
-                  <Link to="/careers" className="nav-link" style={{ color: '#eb3d66' }}>
-                    Career
-                  </Link>
-                </li>
-                <li className="nav-item me-4">
-                  <Link to="/contact" className="nav-link" style={{ color: '#eb3d66' }}>
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
+                    <a
+                      href="#"
+                      className="nav-link"
+                      style={{ color: '#eb3d66' }}
+                    >
+                      Products / Solutions
+                    </a>
+                    {isDropdownOpen && (
+                      <ul className="dropdown-menu show">
+                        <li>
+                          <Link to="/bio-degradable" className="dropdown-item">
+                            BIO DEGRADEABLE
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/offset-printing" className="dropdown-item">
+                            OFFSET PRINTING
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/advise" className="dropdown-item">
+                           AUTO LABEL (SELF ADHESIVE) PRINTING
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/corggoted-cartton" className="dropdown-item">
+                            CORROGATED CARTON
+                          </Link>
+                        </li>
+                      </ul>
+                    )}
+                  </li>
+                  <li className="nav-item me-4">
+                    <Link to="/sustainability" className="nav-link" style={{ color: '#eb3d66' }}>
+                      Sustainability
+                    </Link>
+                  </li>
+                  <li className="nav-item me-4">
+                    <Link to="/news-events" className="nav-link" style={{ color: '#eb3d66' }}>
+                      News & Events
+                    </Link>
+                  </li>
+                  <li className="nav-item me-4">
+                    <Link to="/careers" className="nav-link" style={{ color: '#eb3d66' }}>
+                      Career
+                    </Link>
+                  </li>
+                  <li className="nav-item me-4">
+                    <Link to="/contact" className="nav-link" style={{ color: '#eb3d66' }}>
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </nav>
         </div>
       </header>
+      
+      {/* Backdrop for mobile */}
+      {isSidebarOpen && (
+        <div 
+          className="offcanvas-backdrop show"
+          onClick={() => setIsSidebarOpen(false)}
+        ></div>
+      )}
     </>
   );
 }
